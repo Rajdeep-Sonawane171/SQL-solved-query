20993c3a7e154aee4468591145027e506469f661
Rajdeep-Sonawane171 <113442602+Rajdeep-Sonawane171@users.noreply.github.com>
2023-02-21 18:54:52 +0530

    Update q7_q16.sql

--- q7_q16.sql
+++ q7_q16.sql
@@ -39,4 +39,35 @@ in any order, but exclude duplicates from the answer
 
 Q9. Find the difference between the total number of CITY entries in the table and the number of
 distinct CITY entries in the table
--->
+-->select (count(city)-count(DISTINCT(city)))  as diff from station;
+
+Q10. Query the two cities in STATION with the shortest and longest CITY names, as well as their
+respective lengths (i.e.: number of characters in the name). If there is more than one smallest or
+largest city, choose the one that comes first when ordered alphabetically.
+-->select city,char_LENGTH(city) as max_len from station where char_length(city)=(select max(char_length(city)) from station) order by city limit 1;
+   select city,CHAR_LENGTH(city) as min_len from station where char_length(city)=(select min(CHAR_LENGTH(city)) from station ) order by city limit 1; 
+
+Q11. Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result
+cannot contain duplicates.
+-->select DISTINCT(city) from station where REGEXP_LIKE(city,'^[aeiou]');
+   
+Q12. Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot
+contain duplicates.
+-->select DISTINCT(city) from station where REGEXP_LIKE(city,'[aeiou]$');
+
+Q13. Query the list of CITY names from STATION that do not start with vowels. Your result cannot
+contain duplicates.
+-->select DISTINCT(city) from station where REGEXP_LIKE(city,'^[^aeiou]');
+
+Q14. Query the list of CITY names from STATION that do not end with vowels. Your result cannot
+contain duplicates.
+-->select DISTINCT(city) from station where REGEXP_LIKE(city,'[^aeiou]$');
+
+Q15. Query the list of CITY names from STATION that either do not start with vowels or do not end
+with vowels. Your result cannot contain duplicates.
+-->select distinct(city) from station where REGEXP_like(city,'^[^aeiouAEIOU]|[^aeiouAEIOU]$');
+
+Q16. Query the list of CITY names from STATION that do not start with vowels and do not end with
+vowels. Your result cannot contain duplicates.
+-->select distinct city from station where lower(substr(city,1,1)) not in ('a','e','i','o','u') and lower(substr(city,-1,1)) NOT in ('a','e','i','o','u');
+
