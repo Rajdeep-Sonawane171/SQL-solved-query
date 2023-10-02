f1c1e2c59d90c89042f79247c8101c94b65ba79c
Rajdeep Sonawane <113442602+Rajdeep-Sonawane171@users.noreply.github.com>
2023-10-02 23:13:54 +0530

    Create q1(29).sql

---
+++ q1(29).sql
@@ -0,0 +1,21 @@
+create table tvprogram(
+program_date datetime,
+content_id int,
+channel varchar(30),
+primary key(program_date, content_id)
+);
+create table content(
+content_id varchar(30),
+title varchar(30),
+kids_content enum('Y','N'),
+content_type varchar(30),
+primary key(content_id)
+);
+
+select * from tvprogram;
+select * from content;
+
+-- Write an SQL query to report the distinct titles of the kid-friendly movies streamed in June 2020.
+-- Return the result table in any order
+
+select c.title from tvprogram t inner join content c on t.content_id=c.content_id where month(t.program_date)=6 and c.kids_content='Y' and content_type='Movies' ;
