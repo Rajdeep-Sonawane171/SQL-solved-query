854a752209d0e4ff27ed42dd6113a1c7f56e46c1
Rajdeep Sonawane <113442602+Rajdeep-Sonawane171@users.noreply.github.com>
2023-10-02 23:09:18 +0530

    Create q1(31).sql

---
+++ q1(31).sql
@@ -0,0 +1,19 @@
+create table npv(
+id int,
+year int,
+npv int,
+primary key(id,year)
+);
+create table queries(
+id int,
+year int,
+primary key(id,year)
+);
+select * from npv;
+select * from queries;
+
+-- Q31. Write an SQL query to find the npv of each query of the Queries table.
+-- Return the result table in any order.
+
+select q.id, q.year,ifnull(n.npv,0) as npv
+from queries q left join npv n on q.id=n.id and q.year=n.year;
