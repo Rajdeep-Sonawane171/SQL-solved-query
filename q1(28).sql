3ebbf0d26e48b7b2a0ed5861c029c331eb625f7b
Rajdeep Sonawane <113442602+Rajdeep-Sonawane171@users.noreply.github.com>
2023-10-02 18:14:39 +0530

    Update q1(28).sql

--- q1(28).sql
+++ q1(28).sql
@@ -28,3 +28,20 @@ select * from customers;
 -- Write an SQL query to report the customer_id and customer_name of customers who have spent at
 -- least $100 in each month of June and July 2020.
 -- Return the result table in any order.
+
+
+with cte as 
+         (select c.customer_id, c.name,
+                 o.product_id,o.order_date,
+                 o.quantity,product.price 
+                 from customers c join orders o
+                 on c.customer_id=o.customer_id join product
+                 on product.product_id=o.product_id where month(o.order_date)>5  ),
+ 
+cte1 as (select customer_id,name,
+         sum(quantity*price)as money_spend
+		     from cte group by customer_id,month(order_date)
+         order by name )
+ select customer_id,name
+ from cte1 where money_spend>=100 
+ group by customer_id having count(*)=2
