0d34ae446e57a8c8221786a8199813758caba1c3
Rajdeep Sonawane <113442602+Rajdeep-Sonawane171@users.noreply.github.com>
2023-10-13 22:29:31 +0530

    Update q1(44).sql

--- q1(44).sql
+++ q1(44).sql
@@ -1,1 +1,16 @@
+create table employee(
+id int ,
+name varchar(30),
+department varchar(30),
+manager_id int,
+primary key(id)
+);
+select * from employee;
 
+-- Q44. Write an SQL query to report the managers with at least five direct reports.
+-- Return the result table in any order
+
+select name from (
+with cte as (
+select * from employee where manager_id is not null)
+select e.* from cte c right join employee e on c.manager_id=e.id) d where manager_id is null group by name having count(name)>=5
